30fff5796084ef089abcfdfb78755d054bf40e53
scottmarmul <scottmarmul@gmail.com>
2019-08-20 08:57:08 -0500

    Delete Home.js

--- ScottMarmulstein.com/Home_files/Home.js
+++
@@ -1,9 +0,0 @@
-// Created by iWeb 2.0.4 local-build-20110517
-
-setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
-{return false;}
-function onPageLoad()
-{loadMozillaCSS('Home_files/HomeMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');Widget.onload();performPostEffectsFixups()}
-function onPageUnload()
-{Widget.onunload();}
