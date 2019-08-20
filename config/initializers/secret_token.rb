780daed47719f4de7c0fd8c3d1c914295fafc7aa
scottmarmul <scottmarmul@gmail.com>
2019-08-20 09:08:48 -0500

    Delete secret_token.rb

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Blog::Application.config.secret_token = '3bb2503813fca4db0dc2320dcbd3c7635a934731fe426493512544acc39a74a80b2cb7d382b7d486d3fdf117f9bdca734f3a701ac2a4b01bd0d6a83f536f327a'
